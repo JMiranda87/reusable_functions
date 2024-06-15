bb48661f06146b487d0c82008774f6b4dcf77441
Joseph Miranda <joseph.miranda87@gmail.com>
2024-06-15 14:45:06 -0400

    my second commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Take an array of numbers and return the sum.
 
 function sumArray(arr) {
-  let sum = 0;
+let sum = 0;
     for (let i = 0; i < arr.length; i++) {
         sum += arr[i];
     }
@@ -14,7 +14,7 @@ sumArray([1, 2, 3]);
 // Take an array of numbers and return the average.
 
 function averageArray(arr) {
-  let sum = 0;
+let sum = 0;
     for (let i = 0; i < arr.length; i++) {
         sum += arr[i];
     }
@@ -26,17 +26,51 @@ averageArray([1, 2, 3]);
 // take an array of strings and return the longest string
 
 function longestString(arr) {
-  let longest = "";
-  for (let i = 0; i < arr.length; i++) {
+    let longest = "";
+    for (let i = 0; i < arr.length; i++) {
     if (arr[i].length > longest.length) {
-      longest = arr[i];
+    longest = arr[i];
     } else if (arr[i].length === longest.length) {
-      longest += ", " + arr[i];
+    longest += ", " + arr[i];
     }else {
-      continue;
+    continue;
     }
-  }
-  console.log(longest);
+}
+    console.log(longest);
 }
 
 longestString(["hello", "world", "this", "is", "a", "test"]);
+
+// take an array of SVGStringList, and a number and return an array of strings that are longer than the given number 
+
+function stringsLongerThan(arr, num) {
+let longer = [];
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i].length > num) {
+        longer.push(arr[i]);
+    } else {
+        console.log("No strings longer than " + num);
+    }
+}
+        console.log(longer);
+}
+
+stringsLongerThan(["hello", "world", "this", "is", "a", "test"], 4);
+
+let starting = 0;
+function recursionNumber(n){
+
+starting +=1
+
+if (starting >= n){
+    console.log(starting)
+
+} else {
+    console.log(starting)
+    recursionNumber(n)
+}
+}
+
+recursionNumber(10)
+
+
